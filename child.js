97326d6778da34801f0db333bf2836b6050fb0c3
Jarrett Cruger <jcrugzz@gmail.com>
2015-05-28 22:18:44 -0700

    make it so

---
+++ child.js
@@ -0,0 +1,259 @@
+var Forkee = require('forkee');
+var async = require('async');
+var once = require('one-time');
+var LevelBatch = require('level-batch-stream');
+var BatchStream = require('batch-stream');
+var multilevel = require('multileveldown');
+var sublevel = require('subleveldown');
+var net = require('net');
+var uuid = require('uuid');
+var faker = require('faker');
+var monotonic = require('monotonic-timestamp');
+var parallel = require('concurrent-writable');
+var through = require('through2');
+var streamify = require('stream-array');
+var writeOnly = require('write-only-stream');
+var after = require('after');
+
+function generateArrayWithId(id, len) {
+  return Array.apply(null, new Array(len))
+    .map(function () {
+      return {
+        id: id,
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        descripton: faker.lorem.paragraphs()
+      }
+    });
+}
+
+var actions = {
+  read: read,
+  write: write
+};
+
+var db = multilevel.client();
+var sublevels = {};
+var ended = false;
+var total = 0;
+var all = 0;
+var socket;
+
+var fork = new Forkee()
+  .on('request', function (data, callback) {
+    var fn = once(function () {
+      console.log('final');
+      var args = Array.prototype.slice.call(arguments);
+      ended = true;
+      socket.destroy();
+      db.close(function (err) {
+        if (err) { return callback(err); }
+        callback.apply(null, args);
+      });
+    });
+
+    console.log('what the fuck')
+    connect(data.port || 3000, function () {
+      console.log('connected?');
+      all = data.ids.length;
+      actions[data.action](data, fn);
+    });
+
+  });
+
+function connect(port, callback) {
+
+  socket = net.connect(port);
+
+  socket.on('connect', function () {
+    if (callback) callback();
+  });
+
+  socket.on('error', function () {
+    socket.destroy();
+  });
+
+  socket.on('close', function () {
+    if (!ended) {
+      console.log('socket closed');
+      setTimeout(conect.bind(null, port), 1000);
+    }
+  });
+
+  socket
+    .pipe(db.connect())
+    .pipe(socket);
+
+}
+
+function read(data, callback) {
+  var ids = data.ids;
+  console.log('begin read');
+  sublevels = ids.reduce(function (acc, id) {
+    acc[id] = sublevel(db, id, { valueEncoding: 'json' });
+    return acc;
+  }, {});
+
+  stripe(ids, callback);
+}
+
+function stripe(ids, callback) {
+
+  //
+  // We are done with recursion
+  //
+  if (!ids.length) { return callback(); }
+
+  var newIds = [];
+  async.each(
+    ids,
+    function stripeOne(id, next) {
+      var db = sublevels[id];
+      //
+      // Read the first value from the database
+      //
+      console.log('before read');
+      peek(db, function (err, data) {
+        if (err) { return next(err); }
+        console.log('after read');
+        //
+        // If we returned undefined, we are at the end of the stack
+        //
+        if (!data) {
+          return next();
+        }
+
+        // Do something with the value
+        newIds.push(id);
+
+        //
+        // Delete the value to "pop it off the stack"
+        //
+        console.log('before delete');
+        resilDelete(db, data.key, function (err) {
+          if (err) { return next(err); }
+          console.log('after delete');
+          next();
+        });
+      });
+    },
+    function (err) {
+      if (err) { return callback(err); }
+      // There can be no error here. We handle them upstream
+      stripe(newIds, callback);
+    });
+}
+
+//
+// Pipe a bunch of generated fake data to the multilevel instance based on the
+// IDs passed down
+//
+function write(data, callback) {
+  var ids = data.ids;
+
+  console.log('begin write');
+  sublevels = ids.reduce(function (acc, id) {
+    acc[id] = sublevel(db, id, { valueEncoding: 'json' });
+    return acc;
+  }, {});
+
+  var next = after(ids.length, callback);
+
+  var dests = ids.map(function (id) {
+    return createStream(id, next);
+  });
+
+  //
+  // Pipe recursively while sticking in some setImmediates to give the event
+  // loop some space
+  //
+  recursivePipe(ids, dests);
+
+}
+
+function recursivePipe(ids, dests) {
+
+  var id;
+
+  if (!(id = ids.shift())) {
+    console.log('out of sources');
+    return;
+  }
+
+  var source = streamify(generateArrayWithId(id, 500));
+
+  console.log('pipe source');
+  var dest = dests.shift();
+
+  source
+    .on('error', function (err) {
+      console.error(err);
+      console.log('stream error');
+    })
+    .pipe(dest)
+    .on('error', function (err) {
+      console.error(err);
+      console.log('stream error');
+    })
+
+  setImmediate(function () {
+    recursivePipe(ids, dests);
+  });
+}
+
+function createStream (id, done) {
+  var stream = through.obj();
+  var sub = sublevels[id];
+
+  stream
+    .pipe(through.obj(function (data, enc, cb) {
+      cb(null, {
+        type: 'put',
+        key: monotonic().toString(),
+        value: data
+      });
+    }))
+    .pipe(new BatchStream({ size: 10 }))
+    .pipe(parallel(new LevelBatch(sub), 5))
+    .on('retry', function (data) {
+      console.log('retrying writing %j to disk', data);
+    })
+    .on('finish', function () {
+      console.log('finished writing %s', id);
+      console.log('%d out of %d finished', ++total, all);
+      done();
+    });
+
+    return writeOnly(stream);
+}
+
+/**
+ * A delete that doesn't fail because it retries recursively using setImmediate
+ * so it won't starve i/o
+ */
+function resilDelete(d, key, cb) {
+  d.batch([{ type: 'del', key: key }], function (err) {
+    if (err) {
+      console.error('Error deleting key %s', key);
+      if (!err.notFound) {
+        return void setImmediate(resilDelete, d, key, cb);
+      }
+    }
+
+    cb();
+  });
+}
+
+/*
+ * Return the first element in a database
+ */
+function peek(d, cb) {
+  var data;
+  var fn = once(cb);
+  d.createReadStream({ limit: 1 }).on('data', function (d) {
+    data = d;
+  }).on('error', fn).on('end', function () {
+    fn(null, data);
+  });
+}
+
---
+++ index.js
@@ -0,0 +1,73 @@
+var EE = require('events').EventEmitter;
+var net = require('net');
+var once = require('one-time');
+var Fork = require('fork');
+var level = require('level-hyper');
+var multilevel = require('multileveldown');
+var uuid = require('uuid');
+
+var child = require.resolve('./child');
+
+function generateIds(len) {
+  return Array.apply(null, new Array(len))
+    .map(function () {
+      return uuid();
+    })
+}
+
+function Repro(options) {
+  options = options || {};
+
+  this.path = options.path || 'test.db';
+  this.port = options.port || 3000;
+  this.ids = generateIds(100);
+
+  this._level = level(this.path, { valueEncoding: 'json' });
+
+  this._net = net.createServer(function (socket) {
+
+    socket.on('error', function(err) {
+      socket.destroy();
+    });
+
+    socket.pipe(multilevel.server(this._level)).pipe(socket);
+
+  }.bind(this)).listen(this.port, this._onConnect.bind(this));
+}
+
+Repro.prototype = new EE();
+Repro.prototype.constructor = Repro;
+
+Repro.prototype._onConnect = function (err) {
+  var self = this;
+  if (err) return this.emit('error', err);
+
+  this.spawn('write', function (err) {
+    if (err) return self.emit('error', err);
+    console.log('done writing');
+
+    this.spawn('read', function (err) {
+      if (err) return self.emit('error', err);
+      this.emit('finish');
+    });
+
+  }.bind(this));
+
+};
+
+Repro.prototype.spawn = function (type, callback) {
+  callback = once(callback);
+  return new Fork(child)
+    .fork({ action: type, ids: this.ids }, callback);
+};
+
+var repro = new Repro()
+  .on('error', function (err) {
+    console.error(err);
+    process.exit(1);
+  })
+  .on('finish', function () {
+    console.log('Test case finished');
+    process.exit(0);
+  });
+
